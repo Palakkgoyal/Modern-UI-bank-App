e1fd520ce54506ccf457f20b59258cc1034b75fa
Palak <Palakgoyal0304@gmail.com>
2023-03-11 21:06:22 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { useState } from 'react'
+import { close, logo, menu } from '../assets'
+import { navLinks } from '../constants'
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false)
+
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank"
+        className="w-32 h-8" />
+      <ul className="hidden sm:flex list-none justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-base text-white 
+           ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`}
+          >
+            <a href={`#${nav.id}`}>
+              {nav.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img src={toggle ? close : menu}
+          alt="menu"
+          className="w-7 h-7 object-contain cursor-pointer"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+
+        <div
+          className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="flex list-none flex-col justify-center items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-base text-white mb-4
+                `}
+              >
+                <a href={`#${nav.id}`}>
+                  {nav.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
